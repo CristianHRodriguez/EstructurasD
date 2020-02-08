a230f8f29483c270aa0a8365f519b8da1d4ee5d4
CristianHRodriguez <60761548+CristianHRodriguez@users.noreply.github.com>
2020-02-08 13:35:16 -0500

    Update Fibo_recurs.py

--- Fibo_recurs.py
+++ Fibo_recurs.py
@@ -1,6 +1,6 @@
-x = int(input('Ingrese cuantos numeros desea ver'))
-
 def fib(y):
+x = int(input('Ingrese cuantos numeros desea ver'))
+    
     if y<2:
         return y
     return fib(y-1)+fib(y-2)
